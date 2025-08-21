721e7798332bbe5af960868281bf446866d0e658
iheeya <chdh8987@naver.com>
2025-08-21 15:02:45 +0900

    [FE] fix: 네비게이터 버튼 활성화 수정

--- front/src/shared/navigator.tsx
+++ front/src/shared/navigator.tsx
@@ -1,29 +1,37 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Box, BottomNavigation, BottomNavigationAction } from "@mui/material";
 import { AutoGraphOutlined, EditNoteOutlined } from "@mui/icons-material";
 
 export default function Navigator() {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState<number>(0);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setValue(0);
+    } else if (location.pathname === "/statistics") {
+      setValue(1);
+    }
+  }, [location.pathname]);
+
+  const handleChange = (event: any, newValue: number) => {
+    setValue(newValue);
+  };
 
   const goToSchedule = () => {
     navigate("/");
+    setValue(0);
   };
 
   const goToStatistics = () => {
     navigate("/statistics");
+    setValue(1);
   };
 
   return (
     <Box sx={{ marginTop: 6, width: "100%" }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
+      <BottomNavigation showLabels value={value} onChange={handleChange}>
         <BottomNavigationAction
           label="Schedule"
           icon={<EditNoteOutlined />}
